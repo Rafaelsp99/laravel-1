2a01749bbb0b942e48e369e62e4e804733b8a0e0
Rafael Silva <rspaulaunifran@gmail.com>
2021-07-27 21:51:57 -0300

    EXERCICIO LARAVEL 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\HelloController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get(
     '/', 
-    [HomeController::class, 'index']
+    [HelloController::class, 'hello']
 );
 
